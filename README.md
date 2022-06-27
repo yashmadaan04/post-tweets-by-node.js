47d941bbb0610897ecc5f7309badb378b2e27411
yashmadaan04 <107402802+yashmadaan04@users.noreply.github.com>
2022-06-27 18:06:28 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Publish Text, Gif, & Video Twitter Posts using Node.js
+This repository is for demonstration purposes for anyone going through the [Publish Text, Image, Gif, & Video Twitter Posts using NodeJS](https://coderrocketfuel.com/article/publish-text-image-gif-and-video-twitter-posts-with-node-js) on [CoderRocketFuel.com](https://coderrocketfuel.com).
---
+++ index.js
@@ -0,0 +1,123 @@
+const Twitter = require("twitter")
+const dotenv = require("dotenv")
+const fs = require("fs")
+
+dotenv.config()
+
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+})
+
+// Tweet with Text
+client.post("statuses/update", { status: "tweeted Node.js!" }, function(error, tweet, response) {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(tweet)
+  }
+})
+
+//Tweet with Text and Image
+const imageData = fs.readFileSync("./media/george.jpg") //replace with the path to your image
+
+client.post("media/upload", {media: imageData}, function(error, media, response) {
+  if (error) {
+    console.log(error)
+  } else {
+    const status = {
+      status: "I tweeted from Node.js!",
+      media_ids: media.media_id_string
+    }
+
+    client.post("statuses/update", status, function(error, tweet, response) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log("Successfully tweeted an image!")
+      }
+    })
+  }
+})
+
+//Tweet with Media (GIF or Video)
+const pathToFile = "./media/homer.gif" //for video: "./media/test.mp4"
+const mediaType = "image/gif" //for video: "video/mp4"
+
+const mediaData = fs.readFileSync(pathToFile)
+const mediaSize = fs.statSync(pathToFile).size
+
+initializeMediaUpload()
+  .then(appendFileChunk)
+  .then(finalizeUpload)
+  .then(publishStatusUpdate)
+
+function initializeMediaUpload() {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "INIT",
+      total_bytes: mediaSize,
+      media_type: mediaType
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(data.media_id_string)
+      }
+    })
+  })
+}
+
+function appendFileChunk(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "APPEND",
+      media_id: mediaId,
+      media: mediaData,
+      segment_index: 0
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(mediaId)
+      }
+    })
+  })
+}
+
+function finalizeUpload(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("media/upload", {
+      command: "FINALIZE",
+      media_id: mediaId
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        resolve(mediaId)
+      }
+    })
+  })
+}
+
+function publishStatusUpdate(mediaId) {
+  return new Promise(function(resolve, reject) {
+    client.post("statuses/update", {
+      status: "I tweeted from Node.js!",
+      media_ids: mediaId
+    }, function(error, data, response) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log("Successfully uploaded media and tweeted!")
+        resolve(data)
+      }
+    })
+  })
+}
